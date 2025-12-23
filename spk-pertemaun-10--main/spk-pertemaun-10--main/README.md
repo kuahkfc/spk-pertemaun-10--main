f6212d0044f167486ef7dbf48b5aaac9a19b09cf
Maida <167772229+kuahkfc@users.noreply.github.com>
2025-12-23 12:26:44 +0700

    Add files via upload

---
+++ spk-pertemaun-10--main/spk-pertemaun-10--main/README.md
@@ -0,0 +1,4 @@
+<img width="854" height="536" alt="image" src="https://github.com/user-attachments/assets/7cff4c9f-92d4-4b39-a146-4286555c5bcc" />
+<img width="849" height="536" alt="image" src="https://github.com/user-attachments/assets/db47ea57-15ac-42cf-a132-72988a35b425" />
+<img width="836" height="566" alt="image" src="https://github.com/user-attachments/assets/35e7f95d-00a6-46ae-b0f3-2ea61bdadd9a" />
+<img width="844" height="456" alt="image" src="https://github.com/user-attachments/assets/ec56671f-f9b6-46ca-ace3-7a66d84a53e8" />
---
+++ spk-pertemaun-10--main/spk-pertemaun-10--main/spk-saw-laptop-main/components/sidebar.php
@@ -0,0 +1,17 @@
+<nav id="sidebar" class="active">
+    <h1><a href="index.php" class="logo">LP</a></h1>
+    <ul class="list-unstyled components mb-5">
+        <li>
+            <a href="index.php"><span class="fa fa-laptop"></span> Alternatif Laptop</a>
+        </li>
+        <li class="active">
+            <a href="kriteria.php"><span class="fa fa-tasks"></span> Kriteria Penilaian</a>
+        </li>
+        <li>
+            <a href="penilaian.php"><span class="fa fa-list-ol"></span> Data Penilaian</a>
+        </li>
+        <li>
+            <a href="hitung.php"><span class="fa fa-cogs"></span> Hitung SAW</a>
+        </li>
+    </ul>
+</nav>
